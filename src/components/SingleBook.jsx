250c0f288b50069dd327f3170ca313b895c91baa
Andrea Di paola <andr3a.dipaola@gmail.com>
2023-03-30 17:07:02 +0200

    added some changes

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,28 +1,70 @@
+import { Component } from "react";
 import { Card } from "react-bootstrap";
 
-const SingleBook = (props) => (
-  <Card
-    className="border-0 shadow-lg"
-    key={props.asin}
-    style={{
-      color: "#fff",
-      background: "#282c34"
-    }}
-  >
-    <Card.Img
-      variant="top"
-      src={props.img}
-      style={{ height: "500px", width: "auto", objectFit: "cover" }}
-      className="mb-auto"
-    />
-    <Card.Body>
-      <Card.Text>{props.category}</Card.Text>
-      <Card.Title className="text-truncate mb-4">{props.title}</Card.Title>
-      <Card.Text className="fs-3" variant="primary" style={{ color: "#61DBFB" }}>
-        {props.price} €
-      </Card.Text>
-    </Card.Body>
-  </Card>
-);
+// const SingleBook = (props) => (
+//   <Card
+//     className="border-0 shadow-lg"
+//     key={props.asin}
+//     style={{
+//       color: "#fff",
+//       background: "#282c34"
+//     }}
+//   >
+//     <Card.Img
+//       variant="top"
+//       src={props.img}
+//       style={{ height: "500px", width: "auto", objectFit: "cover" }}
+//       className="mb-auto"
+//     />
+//     <Card.Body>
+//       <Card.Text>{props.category}</Card.Text>
+//       <Card.Title className="text-truncate mb-4">{props.title}</Card.Title>
+//       <Card.Text className="fs-3" variant="primary" style={{ color: "#61DBFB" }}>
+//         {props.price} €
+//       </Card.Text>
+//     </Card.Body>
+//   </Card>
+// );
 
+class SingleBook extends Component {
+  state = {
+    selected: false
+  };
+
+  render() {
+    return (
+      <Card
+        key={`id: ${this.props.asin}`}
+        className="border-0 shadow-lg"
+        style={{
+          color: "#fff",
+          background: "#282c34"
+        }}
+        onClick={() => {
+          console.log("abbiamo cliccato: " + this.props.title);
+          this.setState({ selected: true });
+          console.log(this.state.selected);
+        }}
+
+        // {this.state.selected === true && (
+        //     className = "border border-danger"
+        //   )};
+      >
+        <Card.Img
+          variant="top"
+          src={this.props.img}
+          style={{ height: "500px", width: "auto", objectFit: "cover" }}
+          className="mb-auto"
+        />
+        <Card.Body>
+          <Card.Text>{this.props.category}</Card.Text>
+          <Card.Title className="text-truncate mb-4">{this.props.title}</Card.Title>
+          <Card.Text className="fs-3" variant="primary" style={{ color: "#61DBFB" }}>
+            {this.props.price} €
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    );
+  }
+}
 export default SingleBook;
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -3,7 +3,7 @@ import { Container } from "react-bootstrap";
 const Welcome = () => (
   <div className="p-5 my-5" style={{ color: "#fff", background: "#282c34" }}>
     <Container fluid className="py-5">
-      <h1 className="display-5 fw-bold">&#128218; Andrea's bookshop</h1>
+      <h1 className="display-5 fw-bold">&#128218; Andrea's bookstore</h1>
       <hr className="my-4"></hr>
       <p style={{ color: "#61DBFB" }} className="col-md-8 fs-4 lead">
         SAVE UP TO 70% ON MANY ARTICLES
